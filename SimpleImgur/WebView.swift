b3a761b2ee68b837a3ee68b66018ea8441880b4d
phasefactor <56488589+phasefactor@users.noreply.github.com>
2023-10-05 22:52:21 -0500

    Working, but ugly.  Completely changed from injecting WKUserScript to using WKContentRuleList to block ads, etc.

--- SimpleImgur/WebView.swift
+++ SimpleImgur/WebView.swift
@@ -9,53 +9,196 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    // defined below
-    @State var navDel = WebViewNavigationDelegate()
+    @State var navDel  = WebViewNavigationDelegate()
+    
+    
+  //  var webView: WKWebView
+    
+    init() {
+        let _ = print("INIT")
+        
+        
+    }
+    
     
     func makeUIView(context: Context) -> WKWebView {
-        let webConfiguration = WKWebViewConfiguration()
+        
+        let _ = print("running makeUIView")
+        
+        let config  = WKWebViewConfiguration()
+        let control = WKUserContentController()
+
+        config.userContentController = control
         
         // Needed for the auto-play of video previews to not instantly enter full-screen.
-        webConfiguration.allowsInlineMediaPlayback = true
+        config.allowsInlineMediaPlayback = true
         
-        // Setup a content controller so that we can inject JS to the WKWebView
-        let contentController = WKUserContentController()
+        // add block rules as described:
+        // https://developer.apple.com/documentation/safariservices/creating_a_content_blocker
+        let jsonString = """
+            [
+                {
+                    "trigger": {
+                        "url-filter": ".*doubleclick\\\\.net.*"
+                    },
+                    "action":  {
+                        "type": "block"
+                    }
+                },
+
+                {
+                    "trigger": {
+                        "url-filter": ".*sentry-cdn\\\\.com.*"
+                    },
+                    "action":  {
+                        "type": "block"
+                    }
+                },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*smartadserver\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*assemblyexchange\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*amazon-adsystem\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*ccgateway\\\\.net.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*run\\\\.app.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*facebook\\\\.(net)?(com)?.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*scorecardresearch\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*google-analytics\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*sascdn\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*media-lab\\\\.ai.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*adsafeprotected\\\\.com.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*ad-delivery\\\\.net.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                 {
+                     "trigger": {
+                         "url-filter": ".*cloudfront\\\\.net.*"
+                     },
+                     "action":  {
+                         "type": "block"
+                     }
+                 },
+
+                {
+                    "trigger": {
+                        "url-filter": ".*"
+                    },
+                    "action": {
+                        "type": "css-display-none",
+                        "selector": "div.AdTop, div.BannerAd-cont, div.Ad-adhesive, a.get-app-block"
+                    }
+                }
+            ]
+        """
         
-        // load JS from files
-        
-        // We will load two scripts that the webview will run at different times during initial rendering.
-        // Could be done with a single .atDocumentStart injection creating an event to fire later, but
-        // having two files is nicer.
-        var source = "console.log('failed to load script.js');"
-        do {
-            source = try String(contentsOf: Bundle.main.url(forResource: "atDocumentStartScript", withExtension: "js")!)
-        } catch {}
-  
-        var script = WKUserScript(source: source,
-                                  injectionTime: .atDocumentStart,
-                                  forMainFrameOnly: true)
-        
-        contentController.addUserScript(script)
-        
-        
-        
-        // load JS from files
-        source = "console.log('failed to load script.js');"
-        do {
-            source = try String(contentsOf: Bundle.main.url(forResource: "atDocumentEndScript", withExtension: "js")!)
-        } catch {}
-  
-        script = WKUserScript(source: source,
-                                  injectionTime: .atDocumentEnd,
-                                  forMainFrameOnly: true)
-        
-        contentController.addUserScript(script)
-        
-        // Attach the content controller to the configuration
-        webConfiguration.userContentController = contentController
+        // compile the rules list
+        WKContentRuleListStore.default().compileContentRuleList(forIdentifier: "ContentBlockingRules", encodedContentRuleList: jsonString) { (contentRuleList, error) in
+                
+            if error != nil {
+                let _ = print("ERROR: creating rule list!!!\n ", error as Any)
+                
+                return
+            }
+            let _ = print("rule")
+            // attach compiled rules to config
+            config.userContentController.add(contentRuleList!)
+        }
         
         // make the webView
-        let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        let webView = WKWebView(frame: .zero, configuration: config)
         
         // Assign the NavigationDelegate
         webView.navigationDelegate = navDel
@@ -70,7 +213,9 @@ struct WebView: UIViewRepresentable {
     
  
     func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: URL(string: "https://imgur.com")!)
+        let request = URLRequest(url: URL(string: "https://imgur.io")!)
+        
+        let _ = print("running updateUIView")
         
         webView.load(request)
     }
@@ -83,17 +228,24 @@ struct WebView: UIViewRepresentable {
         func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
             
             if let url = navigationAction.request.url {
-                    if ((url.host?.contains("imgur.com")) != nil) {
-                        decisionHandler(.allow)
-                        return
+                if let host = url.host {
+                    let _ = print("navigating to ", host)
+                    
+                    if host.contains("imgur.com") || host.contains("imgur.io") {
+                            decisionHandler(.allow)
+                            return
                     } else {
-                        // open external links in default browser
-                        UIApplication.shared.open(url)
-                        // then fall through to the .cancel
+                        // kludge to fix a weird redirect
+                        if !(host.hasPrefix("about:") || host.contains("googlesyndication.com") ){
+                            // open external links in default browser
+                            UIApplication.shared.open(url)
+                            // then fall through to the .cancel
+                        }
                     }
                 }
-                
-                decisionHandler(.cancel)
+            }
+            
+            decisionHandler(.cancel)
         }
         
         func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
